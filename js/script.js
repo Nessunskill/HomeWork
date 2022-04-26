8835f90bc55394940d17dd0ea1aea2ec46141791
Artur <kinjal387@gmail.com>
2022-04-26 11:55:23 +0300

    first homework (Movies

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const answer = prompt('Сколько фильмов вы уже посмотрели?');
+let numberOfFilms = answer;
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  acotrs: {},
+  genres: [],
+  privat: false
+}
+
+const firstAnswer = prompt('Один из последних просмотренных фильмов?');
+const firstRating = prompt('На сколько оцените его?');
+
+const secondAnswer = prompt('Один из последних просмотренных фильмов?');
+const secondRating = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[firstAnswer] = firstRating;
+personalMovieDB.movies[secondAnswer] = secondRating;
+
+console.log(personalMovieDB);
